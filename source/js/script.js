a36142c78943038ebfe9e5eda6e0d6a47ba6823e
Eugene <edukalis@gmail.com>
2022-10-06 16:40:13 +0300

    completed all of the tasks

--- source/js/script.js
+++ source/js/script.js
@@ -32,29 +32,53 @@ const isPerfect = (num) => {
             s += i;
         }
     }
-    s === num && s !== 0 ? console.log('Perfect number') : console.log('NOT a perfect number');
+    s === num && s !== 0 ? console.log(`${num} is a PERFECT number`) : console.log(`${num} is NOT a perfect number`);
 };
 
 isPerfect(6);
 
-// 4) Create function which return number of arguments
-const returnArgs = (...args) => {
+// LMS Tasks
+// Min
+// 2) Create function which return number of arguments
+const returnArgsNumber = (...args) => {
     const arr = [];
     arr.push(...args);
-    console.log(`Number of arguments in function: ${arr.length}`);
+    console.log(`Number of arguments in function: ${arr.length} arguments`);
 };
 
-returnArgs(5, 6, 7, 8, 9, 10, 12);
+returnArgsNumber(5, 6, 7, 8, 9, 10, 12);
 
-// 5) Create function which will compare two numbers
+// 3) Create function which will compare two numbers
 const compareFn = (numOne, numTwo) => {
-    numOne < numTwo ? console.log(-1) : numOne > numTwo ? console.log(1) : numOne === numTwo ? console.log(0) : console.log('Some problem happened'); 
+    numOne < numTwo ? console.log(-1) : numOne > numTwo ? console.log(1) : numOne === numTwo ? console.log(0) : console.log('Some problem happened...');
 };
 
+compareFn(5, 10);
 compareFn(15, 5);
+compareFn(1, 1);
 
-// 6) Factorial function
+// 4) Factorial function
 const factorial = (n) => n === 1 ? 1 : n * factorial(n - 1);
 
 console.log('Factorial of 5: ' + factorial(5));
 console.log('Factorial of 10: ' + factorial(10));
+
+// 5) Create function that allows three digits and joins them to one
+const joinDigits = (a, b, c) => {
+    let str = a.toString() + b + c;
+    console.log(`Result of joining digits is: ${str}`);
+};
+
+joinDigits(1, 4, 6);
+
+// Normal
+// 2) Create a function which returns only perfect digits of range
+const minValPerfect = +prompt('Perfect Numbers\nEnter min value');
+const maxValPerfect = +prompt('Perfect Numbers\nEnter max value');
+const checkForPerfect = (minValPerfect, maxValPerfect) => {
+    for (let i = minValPerfect; i <= maxValPerfect; i++) {
+        isPerfect(i);
+    }
+};
+
+checkForPerfect(minValPerfect, maxValPerfect);
